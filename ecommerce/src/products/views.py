1942bb3b5ae298a51b03c4f11228066c81597255
kenziehong <dinhthianhhong.uel@gmail.com>
2019-04-17 17:24:17 +0700

    Section3, Lecture28 - Custom Model Managers

--- ecommerce/src/products/views.py
+++ ecommerce/src/products/views.py
@@ -8,13 +8,17 @@ from .models import Product
 # Create your views here.
 
 class ProductListView(ListView):
-	queryset = Product.objects.all()
+	#queryset = Product.objects.all()
 	template_name = "products/list.html"
 
 	# def get_context_data(self, *args, **kwargs):
 	# 	context = super(ProductListView, self).get_context_data(*args, **kwargs)
 	# 	print(context)
 	# 	return context
+
+	def get_queryset(self, *args, **kwargs):
+		request = self.request
+		return Product.objects.all()
 
 #get_context_data(abc, 123, afddgs, another=abc, abc=123)
 
@@ -27,7 +31,7 @@ def product_list_view(request):
 
 
 class ProductDetailView(DetailView):
-	queryset = Product.objects.all()
+	#queryset = Product.objects.all()
 	template_name = "products/detail.html"
 
 	def get_context_data(self, *args, **kwargs):
@@ -35,6 +39,20 @@ class ProductDetailView(DetailView):
 		print(context)
 		#context['abc'] = 123
 		return context
+
+	def get_object(self, *args, **kwargs):
+		request = self.request
+		pk = self.kwargs.get("pk")
+		instance = Product.objects.get_by_id(pk)
+		if instance is None:
+			raise Http404("Product doesnt exist")
+		return instance
+
+	# def get_queryset(self, *args, **kwargs):
+	# 	request = self.request
+	# 	pk = self.kwargs.get("pk")
+	# 	return Product.objects.filter(pk=pk)	
+
 
 	
 def product_detail_view(request, pk=None, *args, **kwargs ):
@@ -52,11 +70,15 @@ def product_detail_view(request, pk=None, *args, **kwargs ):
 	# except:
 	# 	print("huh?")		
 
-	qs = Product.objects.filter(id=pk)
-	if qs.exists() and qs.count() ==1:
-		instance = qs.first()
-	else:
-		raise Http404("Product doesnt exist")		
+	instance = Product.objects.get_by_id(pk)
+	if instance is None:
+		raise Http404("Product doesnt exist")
+	# print(instance)
+	# qs = Product.objects.filter(id=pk)
+	# if qs.exists() and qs.count() ==1:
+	# 	instance = qs.first()
+	# else:
+	# 	raise Http404("Product doesnt exist")		
 	
 	context = {
 		"object": instance,
