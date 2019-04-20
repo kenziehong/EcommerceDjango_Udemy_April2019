320a6500b6c5c82300b0582d0ece9f0e23efdae1
kenziehong <dinhthianhhong.uel@gmail.com>
2019-04-20 15:21:00 +0700

    Section3, Lecture29,30 - Featured & Custom QuerySets - SlugField & Signals

--- ecommerce/src/products/views.py
+++ ecommerce/src/products/views.py
@@ -6,7 +6,22 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 # Create your views here.
+class ProductFeaturedListView(ListView):
+	template_name = "products/list.html"
 
+	def get_queryset(self, *args, **kwargs):
+		request = self.request
+		return Product.objects.all().featured()
+
+class ProductFeaturedDetailView(DetailView):
+	queryset = Product.objects.all().featured()
+	template_name = "products/featured-detail.html"
+
+	# def get_queryset(self, *args, **kwargs):
+	# 	request = self.request
+	# 	return Product.objects.featured()
+
+	
 class ProductListView(ListView):
 	#queryset = Product.objects.all()
 	template_name = "products/list.html"
@@ -28,6 +43,27 @@ def product_list_view(request):
 		"object_list": queryset
 	}
 	return render(request,"products/list.html",context)
+
+class ProductDetailSlugView(DetailView):
+	queryset = Product.objects.all()
+	template_name = "products/detail.html"
+
+	def get_object(self, *args, **kwargs):
+		request = self.request
+		slug = self.kwargs.get("slug")
+		#instance = get_object_or_404(Product, slug=slug, active=True)
+		try:
+			instance = Product.objects.get(slug=slug, active=True)
+		except Product.DoesNotExist:
+			raise Http404("Not found...")
+		except Product.MultipleObjectsReturned:
+			qs = Product.objects.filter(slug=slug, active=True)
+			instance = qs.first()
+		except:
+			raise Http404("Uhmmm")
+			
+
+		return instance
 
 
 class ProductDetailView(DetailView):
@@ -59,9 +95,9 @@ def product_detail_view(request, pk=None, *args, **kwargs ):
 	#print(args)
 	#print(kwargs)
 	#queryset = Product.objects.all()
-	#instance = Product.objects.get(pk=pk) #id, primary key
-	#instance = get_object_or_404(Product, pk=pk)
+	#instance = Product.objects.get(pk=pk, featured=True) #id, primary key
 
+	#instance = get_object_or_404(Product, pk=pk, featured=True)
 	# try:
 	# 	instance = Product.objects.get(id=pk)
 	# except Product.DoesNotExist:
